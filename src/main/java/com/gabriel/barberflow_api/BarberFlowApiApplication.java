d9afe6aa487a4a31d48999c037922543380b67a9
gabriellaureano <gabrielpc2468@gmail.com>
2026-03-18 13:40:00 -0300

    feat: organizar pastas

---
+++ src/main/java/com/gabriel/barberflow_api/BarberFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.barberflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberFlowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberFlowApiApplication.class, args);
+	}
+
+}
