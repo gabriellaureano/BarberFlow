0b91c30d8fbca1758985ca7fdfc7a6856e0402a8
gabriellaureano <gabrielpc2468@gmail.com>
2026-03-19 14:15:10 -0300

    feat: dtos de request e response criados

---
+++ src/main/java/com/gabriel/barberflow_api/dto/UsuarioRequestDTO.java
@@ -0,0 +1,4 @@
+package com.gabriel.barberflow_api.dto;
+
+public record UsuarioRequestDTO(String nome,String email,String senha) {
+}
---
+++ src/main/java/com/gabriel/barberflow_api/dto/UsuarioResponseDTO.java
@@ -0,0 +1,4 @@
+package com.gabriel.barberflow_api.dto;
+
+public record UsuarioResponseDTO(Long id,String nome,String email) {
+}
